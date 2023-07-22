70d597bfd7b47f794dad3e38a67b718af177ee82
Oscar Martinez <oscar_mar307@outlook.com>
2023-07-22 14:40:29 -0500

    Proyecto de Spring Boot creado.

---
+++ src/main/java/com/example/obrest/ObRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.obrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obrest/ObRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
